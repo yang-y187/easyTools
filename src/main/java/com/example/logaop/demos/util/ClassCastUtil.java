45b6a2e978698915749b8fcd80b570616f25bd9a
wangyangyang137 <wangyangyang137@jd.com>
2024-01-19 17:07:08 +0800

    新增类型转换类

---
+++ src/main/java/com/example/logaop/demos/util/ClassCastUtil.java
@@ -0,0 +1,15 @@
+package com.example.logaop.demos.util;
+
+/**
+ * @author wangyangyang137
+ * @Description: 类型转换工具类
+ * @date 2024-01-19 17:05
+ */
+public class ClassCastUtil {
+
+
+    public static <T> T cast(Object source, Class<T> target) {
+        return target.cast(source);
+    }
+
+}
